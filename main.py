4fd8df0805b603ef3e3cb974eb95c9ce9a1e2f93
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-08 16:27:21 -0400

    Add functions for adding and removing a task

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ def list_tasks(tasks):
     for task in tasks:
         print(task)
 
-list_tasks(get_all_tasks())
+def set_task_list(tasks):
+    with open('task_list.txt', 'w') as f:
+        f.write('\n'.join(tasks))
+
+def add_task(new_task):
+    task_list = get_all_tasks()
+    task_list.append(new_task)
+    set_task_list(task_list)
+
+def remove_task(task_to_remove):
+    task_list = get_all_tasks()
+    task_list = [task for task in task_list if task != task_to_remove]
+    set_task_list(task_list)
+
