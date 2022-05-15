883cf94cc628ee9b8f2f411f469e1bd57bf6ea21
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-15 14:33:17 -0400

    First real routed function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from bottle import route, run
+from repositories.task_repository import TaskRepository, TextFileTaskRepository
+
+repo = TextFileTaskRepository("task_list.txt")
 
 @route("/")
-def hello():
-    return "Bottle says hello."
+def list_tasks(task_repository: TaskRepository = repo):
+    return "<br>".join([str(task) for task in task_repository.get_all_tasks()])
 
 run(host="0.0.0.0", port=8000)
