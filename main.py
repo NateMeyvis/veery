02c98c1317e2e3af6d82e5b5a0e028bfc168a4a4
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-15 10:07:34 -0400

    Add dependency on Bottle. Delete obsolete code and tests.

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-def get_all_tasks():
-    tasks = []
-    with open("task_list.txt") as f:
-        for task_line in f.readlines():
-            tasks.append(task_line.strip())
-    return tasks
+from bottle import route, run
 
+@route("/")
+def hello():
+    return "Bottle says hello."
 
-def list_tasks(tasks):
-    for task in tasks:
-        print(task)
-
-
-def set_task_list(tasks):
-    with open("task_list.txt", "w") as f:
-        f.write("\n".join(tasks))
-
-
-def add_task(new_task):
-    task_list = get_all_tasks()
-    task_list.append(new_task)
-    set_task_list(task_list)
-
-
-def remove_task(task_to_remove):
-    task_list = get_all_tasks()
-    task_list = [task for task in task_list if task != task_to_remove]
-    set_task_list(task_list)
+run(host="0.0.0.0", port=8000)
