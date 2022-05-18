c7db3bce3dbfd6c04794c09ed81857e2eb1f5098
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-18 16:09:22 -0400

    Prepare to add form.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bottle import route, run
+from bottle import request, route, run
 from display.task_display import task_display
 from repositories.task_repository import TaskRepository, TextFileTaskRepository
 
@@ -7,5 +7,15 @@ repo = TextFileTaskRepository("task_list.txt")
 @route("/")
 def list_tasks(task_repository: TaskRepository = repo):
     return task_display(task_repository.get_all_tasks())
+
+@route("/add")
+def add_task(task_repository: TaskRepository = repo):
+    return task_display(task_repository.get_all_tasks())
+
+@route("/add", method="POST")
+def proc_add_task(task_repository: TaskRepository = repo):
+    description = request.forms.get('description')
+    due = request.forms.get('due')
+    raise NotImplementedError
 
 run(host="0.0.0.0", port=8000)
