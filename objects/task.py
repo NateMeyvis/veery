55c38af7bceb0eee17fd817f2dae8479cd3eb654
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-19 10:41:22 -0400

    Augment Task with a CompletionStatus field.

--- objects/task.py
+++ objects/task.py
@@ -1,13 +1,28 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 
 from typing import Optional
+
+
+class CompletionStatus(Enum):
+    OTHER = 0
+    OUTSTANDING = 1
+    COMPLETED = 2
+    WONT_DO = 3
 
 
 @dataclass(frozen=True)
 class Task:
     description: str
     due: Optional[datetime] = None
+    status: CompletionStatus = CompletionStatus.OUTSTANDING
+
+    def _due_description(self):
+        return '' if self.due is None else self.due.isoformat()
+
+    def _completion_status_description(self):
+        return self.status.name
 
     def __str__(self):
-        return f"{self.description}: {'no due date' if self.due is None else self.due.isoformat()}"
+        return f"{self.description}: {self._due_description()} ({self._completion_status_description})"
