36a0b7aa180d9b0ce9dfa41ece94719cc7cd7e5f
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-29 07:55:50 -0400

    Add script for listing tasks

---
+++ list_tasks.py
@@ -0,0 +1,13 @@
+from repositories.task_repository import TaskRepository, SQLiteTaskRepository
+from objects.task import CompletionStatus
+
+repo = SQLiteTaskRepository("tasks.db")
+
+if __name__=="__main__":
+    active = [
+        task
+        for task in repo.get_all_tasks()
+        if task.status == CompletionStatus.OUTSTANDING
+    ]
+    for ix, task in enumerate(active):
+        print(f"{ix}: {task}")
