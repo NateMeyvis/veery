c54b08f77796dbfbbed8c9a98a4ab8c139674b21
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-05-30 11:29:43 -0400

    First pass at Event structure and TaskCoordinator

--- objects/coordinator.py
+++ objects/coordinator.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
 
-class Event(Enum):
+from objects import Task
+from repositories.task_repository import TaskRepository
+
+class Event:
     pass
 
 class TaskCompletion(Event):
-    pass
+    task: Task
+    completed_at: datetime
 
 class TaskCoordinator(ABC):
 
@@ -12,3 +17,12 @@ class TaskCoordinator(ABC):
     def proc_event(event: Event):
         raise NotImplementedError
 
+class KickoffCoordinator(TaskCoordinator):
+    def __init__(self, interval = timedelta(days=1), repo: TaskRepository):
+        self.interval = interval # After a task is completed, schedule the next one <interval> from its completion
+
+    def proc_event(event: Event):
+        if isinstance(event, TaskCompletion):
+            next_task = Task(description = event.task.description,
+                            due = event.completed_at + self.interval)
+            repo.add_task(next_task)
