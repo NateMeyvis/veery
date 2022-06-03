ad1182ca6c851ab959837fac5f642e9ff4a11f10
Nate Meyvis <NateMeyvis@users.noreply.github.com>
2022-06-03 19:50:15 -0400

    Reformatting with Black

--- repositories/coordinator_repository.py
+++ repositories/coordinator_repository.py
@@ -15,17 +15,19 @@ class CoordinatorRepository:
         """Given a task UUID, gives a list of coordinators coordinating it."""
         raise NotImplementedError
 
+
 class SQLiteCoordinatorRepository(CoordinatorRepository):
     CREATE_SCHEMA = f"""CREATE TABLE kickoff_coordinators (uuid text NOT NULL PRIMARY KEY, current_task_uuid text, interval_minutes int)"""
+
     def __init__(self, db_path):
         self.connection = sqlite3.connect(db_path)
 
     @staticmethod
     def _values_tuple_to_kickoff_coordinator(column) -> KickoffCoordinator:
         return KickoffCoordinator(
-            uuid_ = UUID(column[0]),
-            task_uuid_to_track = UUID(column[1]),
-            interval = timedelta(seconds = column[2] * 60)
+            uuid_=UUID(column[0]),
+            task_uuid_to_track=UUID(column[1]),
+            interval=timedelta(seconds=column[2] * 60),
         )
 
     @staticmethod
@@ -33,16 +35,26 @@ class SQLiteCoordinatorRepository(CoordinatorRepository):
         return (
             kickoff_coordinator.uuid.hex,
             kickoff_coordinator.current_task_uuid.hex,
-            (kickoff_coordinator.interval.seconds / 60)
+            (kickoff_coordinator.interval.seconds / 60),
         )
 
     def add(self, task_coordinator: TaskCoordinator):
         if not isinstance(task_coordinator, KickoffCoordinator):
             raise NotImplementedError
-        self.connection.cursor().execute(f"INSERT INTO kickoff_coordinators VALUES (?, ?, ?)", SQLiteCoordinatorRepository._kickoff_coordinator_to_values_tuple(task_coordinator))
+        self.connection.cursor().execute(
+            f"INSERT INTO kickoff_coordinators VALUES (?, ?, ?)",
+            SQLiteCoordinatorRepository._kickoff_coordinator_to_values_tuple(
+                task_coordinator
+            ),
+        )
         self.connection.commit()
 
     def check_task_by_uuid(self, task_uuid: UUID) -> List[TaskCoordinator]:
         # Currently only KickoffCoordinators are implemented
-        results = self.connection.cursor().execute(f"SELECT * FROM kickoff_coordinators WHERE current_task_uuid = '{task_uuid.hex}'")
-        return [SQLiteCoordinatorRepository._values_tuple_to_kickoff_coordinator(result) for result in results]
+        results = self.connection.cursor().execute(
+            f"SELECT * FROM kickoff_coordinators WHERE current_task_uuid = '{task_uuid.hex}'"
+        )
+        return [
+            SQLiteCoordinatorRepository._values_tuple_to_kickoff_coordinator(result)
+            for result in results
+        ]
--- tests/test_coordinator_repository.py
+++ tests/test_coordinator_repository.py
@@ -3,22 +3,31 @@ import pytest
 from objects.coordinator import KickoffCoordinator
 from repositories.coordinator_repository import SQLiteCoordinatorRepository
 
+
 @pytest.fixture
 def repo():
     repo = SQLiteCoordinatorRepository(":memory:")
     repo.connection.execute(SQLiteCoordinatorRepository.CREATE_SCHEMA)
     yield repo
 
+
 def test_repo_smoke(repo):
     assert repo
+
 
 def test_repo_addition(repo, tasks):
     for task in tasks:
         repo.add(KickoffCoordinator(task_uuid_to_track=task.uuid))
     returned = repo.connection.execute("SELECT * FROM kickoff_coordinators").fetchall()
-    coordinators = [SQLiteCoordinatorRepository._values_tuple_to_kickoff_coordinator(column) for column in returned]
+    coordinators = [
+        SQLiteCoordinatorRepository._values_tuple_to_kickoff_coordinator(column)
+        for column in returned
+    ]
     assert len(coordinators) == 3
-    assert set([t.uuid for t in tasks]) == set([c.current_task_uuid for c in coordinators])
+    assert set([t.uuid for t in tasks]) == set(
+        [c.current_task_uuid for c in coordinators]
+    )
+
 
 def test_repo_checking(repo, tasks):
     coordinators = [KickoffCoordinator(task_uuid_to_track=task.uuid) for task in tasks]
